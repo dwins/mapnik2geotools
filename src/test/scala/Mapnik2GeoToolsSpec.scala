ac9bba68e71cdee5388f6b0765a7fbc95f30602f
David Winslow <cdwinslow@gmail.com>
2011-07-06 16:36:59 -0400

    expect 'fontset-name' instead of 'fontset_name'

--- src/test/scala/Mapnik2GeoToolsSpec.scala
+++ src/test/scala/Mapnik2GeoToolsSpec.scala
@@ -99,17 +99,17 @@ object Mapnik2GeoToolsSpec extends Specification with PendingUntilFixed {
 
     "not require a 'type' attribute for shield images" in {
       val transformed =
-        tx(<ShieldSymbolizer name="ref" fontset_name="bold-fonts" size="10" fill="#fff" placement="line" file="&symbols;/mot_shield1.png" min_distance="30" spacing="750"/>)
+        tx(<ShieldSymbolizer name="ref" fontset-name="bold-fonts" size="10" fill="#fff" placement="line" file="&symbols;/mot_shield1.png" min_distance="30" spacing="750"/>)
       transformed must \("Graphic")
     }
 
     "keep the <Size> element outside the ExternalGraphic" in {
-      tx(<ShieldSymbolizer file="foo.png" fontset_name="bold-fonts" height="12" width="12"/>) must \\("Graphic").\("Size")
+      tx(<ShieldSymbolizer file="foo.png" fontset-name="bold-fonts" height="12" width="12"/>) must \\("Graphic").\("Size")
     }
 
     "create halos from only a halo_radius property" in {
       val transformed = 
-        tx(<TextSymbolizer halo_radius="10" name="name" fontset_name="bold-fonts"/>)
+        tx(<TextSymbolizer halo_radius="10" name="name" fontset-name="bold-fonts"/>)
 
       transformed must \\(
         <Halo>
@@ -124,14 +124,14 @@ object Mapnik2GeoToolsSpec extends Specification with PendingUntilFixed {
     "support halo_fill in different formats" in {
       "hex" >> {
         val transformed =
-          tx(<TextSymbolizer halo_fill="#fed7a5" name="name" fontset_name="book-fonts" size="8" fill="black" halo_radius="1" placement="line"/>)
+          tx(<TextSymbolizer halo_fill="#fed7a5" name="name" fontset-name="book-fonts" size="8" fill="black" halo_radius="1" placement="line"/>)
 
         transformed must \("Halo") \("Fill")
       }
 
       "rgba" >> {
         val transformed =
-          tx(<TextSymbolizer halo_fill="rgba(255,255,255,0.25)" name="name" fontset_name="bold-fonts" size="12" fill="#2b2b2b" halo_radius="2" dy="0" placement="line" max_char_angle_delta="40" text_convert="toupper"/>)
+          tx(<TextSymbolizer halo_fill="rgba(255,255,255,0.25)" name="name" fontset-name="bold-fonts" size="12" fill="#2b2b2b" halo_radius="2" dy="0" placement="line" max_char_angle_delta="40" text_convert="toupper"/>)
 
         transformed must \("Halo") \("Fill")
       }
