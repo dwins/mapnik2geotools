75c0c57750ef44e5370b4a0222ba1b40a4e92bdb
David Winslow <cdwinslow@gmail.com>
2011-10-08 10:45:06 -0400

    Move comments to where scaladoc will pick them up

--- src/main/scala/me/winslow/d/mn2gt/Color.scala
+++ src/main/scala/me/winslow/d/mn2gt/Color.scala
@@ -3,24 +3,36 @@ package me.winslow.d.mn2gt
 class Color(color: String) {
   import CSS.NamedColor
 
+  /**
+   * Extractor for RGB colors using the rgb function, e.g. rgba(255,255,255)
+   */
   val Rgb = """rgb\((\d+),\s*(\d+),\s*(\d+)\)""".r
+
+  /**
+   * Extractor for RGBA colors using the rgba function, e.g. rgba(255,255,255,0.25)
+   */
   val Rgba = """rgba\((\d+),\s*(\d+),\s*(\d+),\s*([\d.]+)\)""".r
+
+  /**
+   * Extractor for short hexadecimal RGB strings such as #888, giving each
+   * channel as a String of length 1
+   */
   val ShortHex = """#(\p{XDigit})(\p{XDigit})(\p{XDigit})""".r
+
+  /**
+   * Extractor for hexadecimal RGB strings such as #888888, giving each channel
+   * as a String of length 2
+   */
   val Hex = """#(\p{XDigit}{2})(\p{XDigit}{2})(\p{XDigit}{2})""".r
 
   var hex: String = null
   var alpha: String = null
 
   hex = color match {
-    // named color, e.g. blue
     case NamedColor(hex) => hex
-    // short hex, e.g. #888
     case ShortHex(r, g, b) => Seq("#", r, r, g, g, b, b).mkString
-    // hex, e.g. #888888
     case Hex(r, g, b) => Seq("#", r, g, b).mkString
-    // rgb, e.g. rgb(255,255,255)
     case Rgb(r, g, b) => rgb2hex(r, g, b)
-    // rgba, e.g. rgba(255,255,255,0.25)
     case Rgba(r, g, b, a) =>
       alpha = a
       rgb2hex(r, g, b)
